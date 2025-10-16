5f842ea1191e090ae3af8cba3b798b5ce478906b
deekshaarao <deeekarao@gmail.com>
2025-10-16 14:48:26 +0530

    Add files via upload

---
+++ tower of hanoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void TOH(int n, char s, char d, char t){
+    if (n>1){
+        TOH(n-1,s,t,d);
+        printf("\n move %d disc from %c to %c",n,s,d);
+        TOH(n-1,t,d,s);
+    }
+else{
+        printf("\n move %d disc from %c to %d",n,s,d);
+    }
+}
+int main()
+{
+    int n;
+    printf("read number of discs:");
+    scanf("%d",&n);
+    TOH(n,'s','d','t');
+    return 0;
+ }
+
