9832894debd523a9cd856d9d890a85c4cc0c6232
deekshaarao <deeekarao@gmail.com>
2026-05-06 10:31:09 +0530

    Add files via upload

---
+++ sumofsubsets.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int w[10], x[10], d, n;
+
+void sum_of_subsets(int s, int k, int r)
+{
+    int i;
+    static int count = 1;
+    x[k] = 1;
+    if (w[k] + s == d)
+    {
+        printf("\nSolution %d = ", count++);
+        for (i = 1; i <= n; i++)
+        {
+            if (x[i] == 1)
+                printf("%d\t", w[i]);
+        }
+        printf("\n");
+    }
+    else if (s + w[k] + w[k + 1] <= d)
+        sum_of_subsets(s + w[k], k + 1, r - w[k]);
+    if ((s + r - w[k] >= d) && (s + w[k + 1] <= d))
+    {
+        x[k] = 0;
+        sum_of_subsets(s, k + 1, r - w[k]);
+    }
+}
+
+int main()
+{
+    int i, sum = 0;
+    printf("\nRead number of elements: ");
+    scanf("%d", &n);
+    printf("\nRead elements in ascending order\n");
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &w[i]);
+        sum = sum + w[i];
+    }
+    printf("\nRead value for d: ");
+    scanf("%d", &d);
+    sum_of_subsets(0, 1, sum);
+    return 0;
+}
